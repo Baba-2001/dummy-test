7ea2e827b496124baec5d1aaddc28828ea09e184
Baba-2001 <nitturu.baba@codemancers.com>
2025-02-13 11:53:18 +0530

    migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,8 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[8.0].define(version: 2025_02_06_071310) do
-  # These are extensions that must be enabled in order to support this database
-
   create_table "posts", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
